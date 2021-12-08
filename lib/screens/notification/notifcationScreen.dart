f0fece47d73b8c76aead3ed18509dbee4b595667
BuiThienNhan-nb <buithiennhan2016@gmail.com>
2021-12-08 20:38:47 +0700

    update: notification screen layout

--- lib/screens/notification/notifcationScreen.dart
+++ lib/screens/notification/notifcationScreen.dart
@@ -14,7 +14,7 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      // padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.only(bottom: 60),
       child: Obx(
         () => ListView.builder(
           itemCount: notificationController.listNotifications.length,
